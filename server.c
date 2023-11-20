15b33ce3724b3dee08e48c413930d0e713927b9b
Kristian <kristianjelinek@gmail.com>
2023-11-20 14:31:12 +0100

    minitalk

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: k <k@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 16:05:16 by k                 #+#    #+#             */
-/*   Updated: 2023/11/13 16:06:57 by k                ###   ########.fr       */
+/*   Updated: 2023/11/20 14:30:53 by k                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	create_server_socket()
+int	createServerSocket()
 {
-	int	server_socket;
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	int	serverSocket;
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	
-	if (server_socket == -1)
+	if (serverSocket == -1)
 	{
 		perror("Error in server socket creating!");
 		exit(EXIT_FAILURE);
 	}
-	return (server_socket);
+	return (serverSocket);
 }
