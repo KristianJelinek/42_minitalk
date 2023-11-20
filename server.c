9438c7a4df8ccb0cf7ee3d3153b14aa35f4a7571
Kristian <kristianjelinek@gmail.com>
2023-11-20 14:43:21 +0100

    mt

--- server.c
+++ server.c
@@ -6,29 +6,7 @@
 /*   By: k <k@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 16:05:16 by k                 #+#    #+#             */
-/*   Updated: 2023/11/20 14:30:53 by k                ###   ########.fr       */
+/*   Updated: 2023/11/20 14:43:12 by k                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//Creat server socket
-//Receiving and processing signals from clients
-//Processing incoming data
-//Fast response
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int	createServerSocket()
-{
-	int	serverSocket;
-	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (serverSocket == -1)
-	{
-		perror("Error in server socket creating!");
-		exit(EXIT_FAILURE);
-	}
-	return (serverSocket);
-}
