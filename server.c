2934f0f80f48dff886d3f2e244cf6d9ee5b2dc19
Kristian <kristianjelinek@gmail.com>
2023-11-27 11:58:53 +0100

    mt

--- server.c
+++ server.c
@@ -6,7 +6,30 @@
 /*   By: k <k@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 16:05:16 by k                 #+#    #+#             */
-/*   Updated: 2023/11/20 14:43:12 by k                ###   ########.fr       */
+/*   Updated: 2023/11/27 09:38:32 by k                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[])
+{
+	int	id;
+	id = fork();
+	if (id == 0)
+	{
+		sleep(1);
+	}
+	printf("Current ID: %d, parent ID: %d\n", 
+	getpid(), getppid());
+	if (id != 0)
+	{
+		wait(NULL);
+	}
+	
+	return (0);
+}
